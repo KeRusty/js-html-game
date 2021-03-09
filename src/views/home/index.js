7e26acf83beb1b366b9cbe414d4716ac443695f5
Chathu Abeywickrama <chathu.abey93@gmail.com>
2021-03-09 10:46:32 +0530

    myriad of changes

---
+++ src/views/home/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import Engine from '../../components/engine';
+
+export default function Home() {
+  return (<Engine />);
+}
